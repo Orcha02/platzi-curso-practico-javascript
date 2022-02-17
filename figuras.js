2059244a6eb492ac259b4bb2db6aa5c1c692a8d0
Orcha02 <orcha2002@gmail.com>
2022-02-17 15:58:58 -0500

    Add triangle and circle calculator without design

--- figuras.js
+++ figuras.js
@@ -51,12 +51,13 @@ console.groupEnd();
 
 // Acá interactuamos con el HTML
 
+// CUADRADO
 function calcularPerimetroCuadrado() {
   const input = document.getElementById("inputCuadrado");
   const value = input.value;
 
   const perimetro = perimetroCuadrado(value);
-  alert(perimetro);
+  alert("El perimetro del cuadrado es: " + perimetro);
 }
 
 function calcularAreaCuadrado() {
@@ -64,5 +65,45 @@ function calcularAreaCuadrado() {
   const value = input.value;
 
   const area = areaCuadrado(value);
-  alert(area);
+  alert("El area del cuadrado es: " + area);
+}
+
+// TRIANGULO
+function calcularPerimetroTriangulo() {
+  const input1 = document.getElementById("inputTriangulo1");
+  const value1 = Number(input1.value);
+
+  const input2 = document.getElementById("inputTriangulo2");
+  const value2 = Number(input2.value);
+
+  const Base = document.getElementById("inputTrianguloBase");
+  const valueBase = Number(Base.value);
+
+  const perimetro = perimetroTriangulo(value1, value2, valueBase);
+  alert("El perimetro del triangulo es: " + perimetro);
+}
+
+function calcularAreaTriangulo() {
+  const inputBase = document.getElementById("inputTrianguloBase");
+  const Base = inputBase.value;
+
+  const inputAltura = document.getElementById("inputTrianguloAltura");
+  const Altura = inputAltura.value;
+
+  const area = areaTriangulo(Base, Altura);
+  alert("El areal del triangulo es de: " + area);
+}
+
+// CIRCULO
+function calcularPerimetroCirculo() {
+  const input = document.getElementById("inputCirculo");
+  const value = input.value;
+  const perimetro = perimetroCirculo(value);
+  alert("El perimetro del circulo es: " + perimetro);
+}
+function calcularAreaCirculo() {
+  const input = document.getElementById("inputCirculo");
+  const value = input.value;
+  const area = areaCirculo(value);
+  alert("El area del circulo es: " + area);
 }
