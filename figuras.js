b074d7640fd75199279c7b7de2541fa5fb126a98
Orcha02 <orcha2002@gmail.com>
2022-02-17 15:22:07 -0500

    Add perim and area square calculator

---
+++ figuras.js
@@ -0,0 +1,68 @@
+// Codigo del cuadrado
+console.group("Cuadrados");
+
+function perimetroCuadrado(lado) {
+  return lado * 4;
+}
+
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+
+console.groupEnd();
+
+// Codigo del triangulo
+console.group("Triangulos");
+
+function perimetroTriangulo(lado1, lado2, base) {
+  return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
+}
+
+console.groupEnd();
+
+// Codigo del circulo
+console.group("Circulos");
+
+// Diametro
+function diametroCirculo(radio) {
+  return radio * 2;
+}
+
+// Pi
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+// Circunferencia
+function perimetroCirculo(radio) {
+  const diametro = diametroCirculo(radio);
+  return diametro * PI;
+}
+
+// Area
+function areaCirculo(radio) {
+  return radio * radio * PI;
+}
+
+console.groupEnd();
+
+// Acá interactuamos con el HTML
+
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById("inputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+  const input = document.getElementById("inputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area);
+}
